5d5f8923c6407f7682178aaf6f15d2f2d4888b3a
Icaro Heimig <icaro.mh@gmail.com>
2017-04-21 12:40:39 -0300

    feat: add login screen and sign-up

--- www/js/app.js
+++ www/js/app.js
@@ -1,86 +1,149 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $ionicPopup, $ionicHistory, $window, $rootScope, $state, AuthService, AUTH_EVENTS) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+    if(AuthService.hasToken()){
+      $state.go('tab.disciplinas');
+      // $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+      // AuthService
+      //   .revalidate()
+      //   .then(function(data){})
+      //   .catch(function(data){
+        // });
+    }else{
+      $state.go('login');
+    }
+
+  })
+
+  $ionicPlatform.registerBackButtonAction(function(event) {
+    if ($state.current.name === 'app') {
+      $ionicPopup.confirm({
+        title: 'Alerta!',
+        template: 'Você tem certeza que deseja sair?'
+      }).then(function(res) {
+        if (res) {
+          ionic.Platform.exitApp();
+        }
+      });
+    }else{
+      $ionicHistory.goBack();
+    }
+  }, 100);
+
+  $ionicPlatform.ready(function() {
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
+    $rootScope.$on('$stateChangeStart', function (event, next, nextParams, fromState) {
+      if ('data' in next && 'authorizedRoles' in next.data) {
+        var authorizedRoles = next.data.authorizedRoles;
+        if (!AuthService.isAuthorized(authorizedRoles)) {
+          event.preventDefault();
+          $state.go($state.current, {}, {reload: true});
+          $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+        }
+      }
+
+      if (!AuthService.isAuthenticated()) {
+        if (next.name !== 'login') {
+          event.preventDefault();
+          $state.go('login');
+        }
+      }else{
+        AuthService.loadUserCredentials();
+      }
+    });
   });
 })
+
+// .constant('API_URL', 'https://api.easyac.xyz')
+.constant('API_URL', 'http://localhost:3000')
 
 .config(function($ionicConfigProvider) {
     $ionicConfigProvider.tabs.position('bottom');
 })
 
+.constant('AUTH_EVENTS', {
+  notAuthenticated: 'auth-not-authenticated',
+  notAuthorized: 'auth-not-authorized'
+})
+
+.constant('USER_ROLES', {
+  admin: 'admin_role',
+  public: 'public_role'
+})
+
+.config([
+  '$httpProvider',
+  function ($httpProvider){
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
+    $httpProvider.interceptors.push('httpRequestInterceptor');
+    $httpProvider.interceptors.push('AuthInterceptor');
+  }
+])
+
+.factory('AuthInterceptor', function ($rootScope, $q, AUTH_EVENTS) {
+  return {
+    responseError: function (response) {
+      $rootScope.$broadcast({
+        401: AUTH_EVENTS.notAuthenticated,
+        403: AUTH_EVENTS.notAuthorized
+      }[response.status], response);
+      return $q.reject(response);
+    }
+  };
+})
+
+.factory('httpRequestInterceptor', function () {
+  return {
+    request: function (config) {
+      var token = localStorage.getItem('6wnk6myVEw5nZ4D4YrLff3gnCER3RB4L');
+      if(token){
+        config.headers.Authorization = 'Bearer ' + token;
+      }
+      return config;
+    }
+  };
+})
+
+
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
+  .state('login', {
+    url: '/',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
 
-  // setup an abstract state for the tabs directive
+  .state('cadastro', {
+    url: '/cadastre-se',
+    templateUrl: 'templates/cadastre-se.html',
+    controller: 'CadastroCtrl'
+  })
+
   .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
-  // Each tab has its own nav history stack:
-  .state('tab.faltas', {
-    url: '/faltas',
-    views: {
-      'tab-faltas': {
-        templateUrl: 'templates/tab-faltas.html',
-        controller: 'FaltasCtrl'
-      }
-    }
-  })
-
-  .state('tab.notas', {
-    url: '/notas',
-    views: {
-      'tab-notas': {
-        templateUrl: 'templates/tab-notas.html',
-        controller: 'NotasCtrl'
-      }
-    }
-  })
-
   .state('tab.disciplinas', {
-      url: '/disciplinas',
-      views: {
-        'tab-disciplinas': {
-          templateUrl: 'templates/tab-faltas.html',
-          controller: 'FaltasCtrl'
-        }
+    url: '/disciplinas',
+    views: {
+      'tab-disciplinas': {
+        templateUrl: 'templates/tab-disciplinas.html',
+        controller: 'DisciplinasCtrl'
       }
-    })
-    .state('tab.disciplinas-detail', {
-      url: '/disciplinas/:disciplinaId',
-      views: {
-        'tab-disciplinas': {
-          templateUrl: 'templates/disciplina-detail.html',
-          controller: 'DisciplinaDetailCtrl'
-        }
-      }
-    })
+    }
+  })
 
   .state('tab.account', {
     url: '/account',
@@ -93,6 +156,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/faltas');
-
+  $urlRouterProvider.otherwise(function ($injector) {
+    var $state = $injector.get('$state');
+    $state.go('login');
+  });
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,140 @@
 
 angular.module('starter.controllers', [])
-.controller('FaltasCtrl', function($scope, Chats) {
-  var all = Chats.all();
+
+.controller('AppCtrl',
+  function($scope, $ionicPopup, AuthService, AUTH_EVENTS) {
+    $scope.username = AuthService.username();
+
+    $scope.$on(AUTH_EVENTS.notAuthorized, function() {
+      $ionicPopup.alert({
+        title: 'Não autorizado!',
+        template: 'Você nao está autorizado a acessar este recurso.'
+      });
+    });
+
+    $scope.$on(AUTH_EVENTS.notAuthenticated, function() {
+      AuthService.logout();
+      $state.go('login');
+      $ionicPopup.alert({
+        title: 'Sessão perdida!',
+        template: 'Desculpe, voce terá que logar noavemente.'
+      });
+    });
+  })
+
+  .controller('LoginCtrl',
+    function($scope, $state, $location, AuthService, $ionicPopup, $ionicLoading){
+      $scope.data = {
+        username : '',
+        password : ''
+      };
+
+      $scope.isFilled = function(valid){
+        return valid === true ? 'valid' : '';
+      };
+
+      $scope.login = function(data) {
+        $ionicLoading.show({template: 'Carregando'});
+
+        AuthService.login(data).then(function() {
+          $ionicLoading.hide();
+          $state.go('app', {}, {reload: true});
+        }, function() {
+          $ionicLoading.hide();
+          $ionicPopup.alert({
+            title: 'Login Falhou!',
+            template: 'Por favor, verifique os dados informados!'
+          });
+        });
+      };
+    })
+
+    .controller('CadastroCtrl',
+      function($scope, $state, $location, AuthService, $ionicPopup, $ionicLoading){
+        $scope.data = {
+          username : '',
+          password : ''
+        };
+
+        $scope.isFilled = function(valid){
+          return valid === true ? 'valid' : '';
+        };
+
+        $scope.login = function(data) {
+          $ionicLoading.show({template: 'Carregando'});
+
+          AuthService.login(data).then(function() {
+            $ionicLoading.hide();
+            $state.go('app', {}, {reload: true});
+          }, function() {
+            $ionicLoading.hide();
+            $ionicPopup.alert({
+              title: 'Login Falhou!',
+              template: 'Por favor, verifique os dados informados!'
+            });
+          });
+        };
+      })
+
+.controller('LoginCtrl',
+  function($scope, $state, $location, AuthService, $ionicPopup, $ionicLoading){
+    $scope.data = {
+      email : '',
+      password : ''
+    };
+
+    $scope.isFilled = function(valid){
+      return valid === true ? 'valid' : '';
+    };
+
+    $scope.login = function(data) {
+      $ionicLoading.show({template: 'Carregando'});
+
+      AuthService.login(data)
+      .then(function() {
+        $ionicLoading.hide();
+        $state.go('tab.disciplinas', {}, {reload: true});
+      }, function() {
+        $ionicLoading.hide();
+        $ionicPopup.alert({
+          title: 'Login Falhou!',
+          template: 'Por favor, verifique os dados informados!'
+        });
+      });
+    };
+  })
+
+.controller('CadastroCtrl',
+  function($scope, $state, $location, AuthService, $ionicPopup, $ionicLoading){
+    $scope.data = {
+      email : '',
+      password : ''
+    };
+
+    $scope.isFilled = function(valid){
+      return valid === true ? 'valid' : '';
+    };
+
+    $scope.create = function(data) {
+      $ionicLoading.show({template: 'Carregando'});
+
+      AuthService.create(data)
+      .then(function() {
+        $ionicLoading.hide();
+        $state.go('login', {}, {reload: true});
+      }, function() {
+        $ionicLoading.hide();
+        $ionicPopup.alert({
+          title: 'Falha!',
+          template: 'Desculpe, houve um problema ao cadastrar o usuário!'
+        });
+      });
+    };
+  })
+
+
+.controller('DisciplinasCtrl', function($scope, Disciplinas) {
+  var all = Disciplinas.all();
   $scope.faltas = all;
 
   $scope.periodos = all
@@ -10,31 +143,6 @@ angular.module('starter.controllers', [])
     .sort();
 
   $scope.filter = $scope.periodos[$scope.periodos.length - 1];
-})
-
-.controller('NotasCtrl', function($scope, Notas) {
-  var all = Notas.all();
-  var model = {};
-
-  all.forEach(function(f){
-    var key = f.ano + '/' + f.semestre;
-
-    if(!model[key]){
-      model[key] = [];
-    }
-
-    model[key].push(f);
-  });
-
-  $scope.faltas = model;
-})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  alert(1)
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
 })
 
 .controller('DisciplinaDetailCtrl', function($scope, $stateParams, Notas) {
